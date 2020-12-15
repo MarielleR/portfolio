10970fddefc2fb5538b48dfa1a3e1c0a047aa3b2
Marielle <marielle@theredmoon.ca>
2020-12-14 22:09:34 -0500

    toggle switch functional and styles added

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-AOS.init();
+
+$(document).ready(function () {
+
+    // Switch to Light Mode
+    $("h1").on("click", function () {
+        $("body").toggleClass("light-mode-test");
+        console.log('clickedddd')
+    });
+
+
+});
+
+
+
+// AOS.init();
