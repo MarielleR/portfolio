b6928add27c6a89f8a0fe99d2a7b30b9c8583681
Marielle <marielle@theredmoon.ca>
2020-12-15 15:00:29 -0500

    created event listener for projection section and added more styles

--- script.js
+++ script.js
@@ -2,11 +2,15 @@
 $(document).ready(function () {
 
     // Switch to Light Mode
-    $("input:checked").on("click", function () {
+    $("header input:checked").on("click", function () {
         $("body").toggleClass("light-mode-test");
         console.log('clickedddd')
     });
 
+    $("#read-one").on("click", function () {
+        $("#project-one-text").toggleClass("hidden");
+        console.log('click on read one')
+    });
 
 });
 
