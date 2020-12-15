ac67e23eeae76c2c5f526b3fa846a129873f1a7a
Marielle <marielle@theredmoon.ca>
2020-12-15 12:32:34 -0500

    completed header styles/toggle styling

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 $(document).ready(function () {
 
     // Switch to Light Mode
-    $("h1").on("click", function () {
+    $("input:checked").on("click", function () {
         $("body").toggleClass("light-mode-test");
         console.log('clickedddd')
     });
